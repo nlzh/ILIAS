ab008ec0f75c1f210fcaa632c80eb5e6c8f06f32
Nils Haagen <nhaagen@cat06.de>
2017-12-15 15:36:23 +0100

    simplify examples for sidebar

--- src/UI/examples/Layout/Page/page.php
+++ src/UI/examples/Layout/Page/page.php
@@ -6,8 +6,42 @@ function page() {
 	$renderer = $DIC->ui()->renderer();
 
 	$content = $f->legacy("some content");
+
 	$page = $f->layout()->page($content)
-	;
+		->withTopbar(page_getTopbar($f))
+		->withSidebar(page_getSidebar($f))
+		;
 
 	return $renderer->render($page);
+}
+
+
+function page_getTopbar($f) {
+	$topbar = $f->layout()->topbar();
+	return $topbar;
+}
+
+function page_getSidebar($f) {
+	$mf = $f->maincontrols()->menu();
+
+	$icon = $f->icon()->standard('sidebar_trigger', 'Example')
+		->withSize('medium');
+
+	$sidebar = $f->layout()->sidebar();
+
+	foreach(range(1,4) as $c){
+		$i = (string)$c;
+		$planks = array(
+			$mf->plank($f->legacy("Plank 1 - $i")),
+			$mf->plank($f->legacy("Plank 2 - $i"))
+		);
+		$slate =$mf->slate($planks);
+		$button = $f->button()->iconographic($icon->withAbbreviation($i), "Button $i", '#');
+		$sidebar = $sidebar->withEntry($button, $slate);
+	}
+
+	$extra_button = $f->button()->iconographic($icon->withAbbreviation('E'), 'Extra', '#');
+	$sidebar = $sidebar->withEntry($extra_button);
+
+	return $sidebar;
 }
--- src/UI/examples/Layout/SideBar/sidebar.php
+++ src/UI/examples/Layout/SideBar/sidebar.php
@@ -3,41 +3,27 @@
 function sidebar() {
 	global $DIC;
 	$f = $DIC->ui()->factory();
+	$mf = $f->maincontrols()->menu();
 	$renderer = $DIC->ui()->renderer();
 
-	$icon = $f->icon()->standard('sidebar_trigger', 'Example')->withSize('medium');
+	$icon = $f->icon()->standard('sidebar_trigger', 'Example')
+		->withSize('medium');
 
-	$planks1 = array(
-		$f->maincontrols()->menu()->plank($f->legacy("Plank 1")),
-		$f->maincontrols()->menu()->plank($f->legacy("Plank 2"))
-	);
-	$slate1 =$f->maincontrols()->menu()->slate($planks1);
-	$button1 = $f->button()->iconographic($icon->withAbbreviation('I'), 'Button1', '#');
+	$sidebar = $f->layout()->sidebar();
 
-
-	$planks2 = array(
-		$f->maincontrols()->menu()->plank($f->legacy("Plank 3")),
-		$f->maincontrols()->menu()->plank($f->legacy("Plank 4"))
-	);
-	$slate2 =$f->maincontrols()->menu()->slate($planks2);
-	$button2 = $f->button()->iconographic($icon->withAbbreviation('II'), 'Button2', '#');
-
-
-	$planks3 = array(
-		$f->maincontrols()->menu()->plank($f->legacy("Plank 5")),
-		$f->maincontrols()->menu()->plank($f->legacy("Plank 6"))
-	);
-	$slate3 =$f->maincontrols()->menu()->slate($planks3);
-	$button3 = $f->button()->iconographic($icon->withAbbreviation('III'), 'Button3', '#');
+	foreach(range(1,4) as $c){
+		$i = (string)$c;
+		$planks = array(
+			$mf->plank($f->legacy("Plank 1 - $i")),
+			$mf->plank($f->legacy("Plank 2 - $i"))
+		);
+		$slate =$mf->slate($planks);
+		$button = $f->button()->iconographic($icon->withAbbreviation($i), "Button $i", '#');
+		$sidebar = $sidebar->withEntry($button, $slate);
+	}
 
 	$extra_button = $f->button()->iconographic($icon->withAbbreviation('E'), 'Extra', '#');
-
-
-	$sidebar = $f->layout()->sidebar()
-		->withEntry($button1, $slate1)
-		->withEntry($button2, $slate2)
-		->withEntry($button3, $slate3)
-		->withEntry($extra_button);
+	$sidebar = $sidebar->withEntry($extra_button);
 
 	return $renderer->render($sidebar);
 }
