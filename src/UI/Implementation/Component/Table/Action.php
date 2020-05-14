e909280c1ff6b392fa068faf35e50292c078454f
Nils Haagen <nhaagen@cat06.de>
2020-05-14 12:24:13 +0200

    UI, DataTable - Actions revised

---
+++ src/UI/Implementation/Component/Table/Action.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace ILIAS\UI\Implementation\Component\Table;
+
+use ILIAS\UI\Component\Table as T;
+use ILIAS\UI\Component\Signal;
+use ILIAS\Data\URI;
+
+abstract class Action implements T\Action
+{
+    protected $label;
+    protected $parameter;
+    protected $target;
+    protected $parameter_name;
+
+    public function __construct(
+        string $label,
+        string $parameter_name,
+        mixed $target,
+        mixed $scope = T\Action::SCOPE_BOTH
+    ) {
+        $this->label = $label;
+        $this->parameter_name = $parameter_name;
+
+        if( ! ($target instanceof URI || $target instanceof Signal)) {
+            throw new \InvalidArgumentException("target must be URL or Signal",1);
+        }
+        $this->target = $target;
+
+        if(in_array($scope, [
+            T\Action::SCOPE_BOTH,
+            T\Action::SCOPE_SINGLE,
+            T\Action::SCOPE_MULTI
+        ])) {
+            throw new \InvalidArgumentException("not a valid action-scope",1);
+        }
+        $this->scope = $scope;
+    }
+
+    public function getLabel() : string
+    {
+        return $this->label;
+    }
+
+    public function getParameterName() : string
+    {
+        return $this->parameter_name;
+    }
+
+    /*
+     * @inheritdoc
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    final public function getScope()
+    {
+        return $this->scope;
+    }
+
+}
