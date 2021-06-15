4396d22ba187c08054efa474db0a1d3fb44415c2
Nils Haagen <nhaagen@cat06.de>
2021-06-15 12:43:57 +0200

    PRG: update user-progress db, rename/add mail_sent-fields

--- setup/sql/7_hotfixes.php
+++ setup/sql/7_hotfixes.php
@@ -896,3 +896,23 @@ if (!$ilDB->tableColumnExists("prg_usr_progress", "individual")) {
 <?php
 $ilDB->manipulate("UPDATE prg_usr_progress SET individual = 1 WHERE last_change_by IS NOT NULL");
 ?>
+
+<#47>
+<?php
+$old = "risky_to_fail_mail_send";
+$new = "sent_mail_risky_to_fail";
+$table = "prg_usr_progress";
+if ($ilDB->tableColumnExists($table, $old) && !$ilDB->tableColumnExists($table, $new)) {
+    $ilDB->renameTableColumn($table, $old, $new);
+}
+?>
+
+<#48>
+<?php
+if (!$ilDB->tableColumnExists("prg_usr_progress", "sent_mail_expires")) {
+    $ilDB->addTableColumn("prg_usr_progress", "sent_mail_expires", [
+            "type" => "timestamp",
+            "notnull" => false
+    ]);
+}
+?>
